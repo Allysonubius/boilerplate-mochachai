616c6c25f40780d78a1c680e046eefa199d9dfa4
allyson <allyson.brandao@venhapranuvem.com.br>
2021-03-23 20:04:53 -0300

    Famous Italian Explorers completed

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -72,23 +72,36 @@ suite("Functional Tests", function() {
     });
 
     const Browser = require("zombie");
+    Browser.site = 'localhost:3000';
 
-    suite("Functional Tests with Zombie.js", function() {
-
+    suite("e2e Testing with Zombie.js", function() {
+        const browser = new Browser();
+        suiteSetup(function(done) {
+            return browser.visit('/', done);
+        });
         suite('"Famous Italian Explorers" form', function() {
             // #5
             test('submit "surname" : "Colombo" - write your e2e test...', function(done) {
-                browser.fill("surname", "Colombo").pressButton("submit", function() {
-                    assert.fail();
+                browser.fill('surname', 'Colombo').pressButton('submit', function() {
+                    browser.assert.success();
+                    browser.assert.text('span#name', 'Cristoforo');
+                    browser.assert.text('span#surname', 'Colombo');
+                    browser.assert.element('span#dates', 1);
 
                     done();
                 });
             });
             // #6
             test('submit "surname" : "Vespucci" - write your e2e test...', function(done) {
-                assert.fail();
+                // fill the form, and submit.
+                browser.fill('surname', 'Vespucci').pressButton('submit', function() {
+                    browser.assert.success();
+                    browser.assert.text('span#name', 'Amerigo');
+                    browser.assert.text('span#surname', 'Vespucci');
+                    browser.assert.element('span#dates', 1);
 
-                done();
+                    done();
+                });
             });
         });
     })
